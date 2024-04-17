36664dd511be3cf9e1c12e4aa144208039cff413
funinng <53615993+funinng@users.noreply.github.com>
2024-04-17 15:05:07 +0800

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,14 @@
+{
+  "code" : 0,
+  "data" : [
+    {
+      "id" : "981263334416515072",
+      "insertPhotoUrl" : "https://pic3.zhimg.com/v2-f24882965102431d856bed4273fc28f6_r.jpg",
+      "advertisingLinkUrl" : "https://datasink.leoao.com/t/ees",
+      "advertisingActivityName" : "自研瑜伽服-开机屏"
+    }
+  ],
+  "msg" : null,
+  "act" : 0,
+  "page" : null
+}
